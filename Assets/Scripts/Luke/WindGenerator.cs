53d46eda50b925274bbb6bd3f2bea10ffb18a9f5
Luke Lazzaro <makorenwastaken@gmail.com>
2020-07-29 11:59:24 +1000

    Create wind scripts
    Unfinished, do not merge

---
+++ Assets/Scripts/Luke/WindGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Where does this component go? Need a wind object to attach this script to, where the designers can define the direction of the wind
+        // Then you need a component for objects that can be affected by wind to put the movement logic
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Luke/WindObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindObject : MonoBehaviour
+{
+    [SerializeField] private GameObject windGenerator;
+
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Since player does not use a dynamic rigidbody, you need to use vector maths to calculate which direction to MovePosition to
+    }
+}
