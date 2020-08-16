adfd324ed47bede920a7fffc2a1ebdc21915f34b
Luke Lazzaro <makorenwastaken@gmail.com>
2020-08-17 09:49:45 +1000

    Remove import that caused weird behaviour on designer build

--- Assets/Scripts/Luke/SaveManager.cs
+++ Assets/Scripts/Luke/SaveManager.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.SqlServer.Server;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
