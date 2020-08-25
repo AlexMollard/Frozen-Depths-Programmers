a005d5a450736832f75087714d64d8f49b4dc054
Michael <michaelsweetman888@gmail.com>
2020-08-25 16:29:46 +1000

    Changed Fuel Display to be a bar on the tool itself
    moved fuel display to new FuelBar.cs script
    changed raycast in tool.cs to be a spherecast to more consistently hit small pieces of ice
    added minimum freeze distance to minimise the chance of the player creating ice in themselves

---
+++ Assets/Scripts/Michael/FuelBar.cs
@@ -0,0 +1,32 @@
+﻿/*
+    File name: Tool.cs
+    Author: Michael Sweetman
+    Summary: Manages the Fuel Bar display on the tool canvas
+    Creation Date: 25/08/2020
+    Last Modified: 25/08/2020
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelBar : MonoBehaviour
+{
+    public Tool tool;
+    public float fuelBarMaxWidth = 300.0f;
+    public float fuelBarMinWidth = 0.0f;
+
+    RectTransform bar;
+
+    private void Start()
+    {
+        bar = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bar.sizeDelta = new Vector2(fuelBarMinWidth + (tool.toolFuel / tool.capacity * (fuelBarMaxWidth - fuelBarMinWidth)), bar.sizeDelta.y);
+    }
+}
--- Assets/Scripts/Michael/Tool.cs
+++ Assets/Scripts/Michael/Tool.cs
@@ -3,19 +3,20 @@
     Author: Michael Sweetman
     Summary: Determines a point on the ice mesh the player wants burnt/frozen. Manages a fuel to limit the use of ice creation.
     Creation Date: 21/07/2020
-    Last Modified: 18/08/2020
+    Last Modified: 25/08/2020
 */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Tool : MonoBehaviour
 {
     [Header("Tool Use")]
-    public float range = 10.0f;
-    public float radius = 10.0f;
+    public float minimumFreezeDistance = 2.0f;
+    public float maxRange = 10.0f;
+    public float beamRadius = 0.5f;
+    public float effectRadius = 10.0f;
 
     [Header("Fuel Economy")]
     [HideInInspector]
@@ -24,9 +25,6 @@ public class Tool : MonoBehaviour
     public float FuelLossRate = 100.0f;
     public float capacity = 1000.0f;
     public float toolStrength = 0.1f;
-
-    [Header("FuelDisplay")]
-    public Text fuelDisplay;
 
     [Header("Camera")]
     public Camera playerCamera;
@@ -39,14 +37,15 @@ public class Tool : MonoBehaviour
             // cast a ray forward from the center of the player camera viewport
             Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f,0.5f,1.0f));
             RaycastHit hit;
+
             // if the ray hit a game object with the tag "Ice"
-            if (Physics.Raycast(ray, out hit, range) && hit.transform.tag == "Ice")
+            if (Physics.SphereCast(ray, beamRadius, out hit, maxRange) && hit.transform.tag == "Ice")
             {
                 // if the mouse was left clicked
                 if (Input.GetMouseButton(0))
                 {
                     // burn the ice at the point of the collision. If this succeeds
-                    if (hit.transform.GetComponent<EditableTerrain>().Burn(hit.point, radius, toolStrength))
+                    if (hit.transform.GetComponent<EditableTerrain>().Burn(hit.point, effectRadius, toolStrength))
                     {
                         // increase the fuel by the fuel gain rate per second
                         toolFuel += Time.deltaTime * FuelGainRate;
@@ -58,11 +57,11 @@ public class Tool : MonoBehaviour
                         }
                     }
                 }
-                // if the mouse was right clicked
-                else
+                // if the mouse was right clicked and the collision point was beyond the minimum creation distance
+                else if (hit.distance >= minimumFreezeDistance)
                 {
                     // freeze the ice at the point of the collision. If this succeeds
-                    if (hit.transform.GetComponent<EditableTerrain>().Freeze(hit.point, radius, toolStrength))
+                    if (hit.transform.GetComponent<EditableTerrain>().Freeze(hit.point, effectRadius, toolStrength))
                     {
                         // decrease the the fuel by the fuel loss rate per second
                         toolFuel -= Time.deltaTime * FuelLossRate;
@@ -75,8 +74,6 @@ public class Tool : MonoBehaviour
                     }
                 }
             }
-            // update the fuel display text
-            fuelDisplay.text = toolFuel.ToString("F2") + "mL";
         }
     }
 }
